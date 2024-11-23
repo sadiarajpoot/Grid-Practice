f93f40f98656dca81f2dc5a33b0eeb0dfbadb8be
sadiarajpoot <sadiakham985@gmail.com>
2024-11-23 10:17:32 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,45 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+<div>
+  <div className="grid grid-rows-[60px_300px_200px_100px] sm:grid-rows-[30px_150px_100px_50px] md:grid-rows-[50px_200px_200px_100px] grid-cols-3 sm:grid-cols-2 md:grid-cols-3 bg-red-800 gap-3 p-9 content-center text-center font-bold text-2xl m-auto">
+    <div className="col-span-3 sm:col-span-2 md:col-span-3 bg-gradient-to-br from-pink-700 via-blue-400 to-emerald-600">Header</div>
+    <div className="row-span-2 sm:row-span-1 md:row-span-2 bg-gradient-to-br from-pink-400 via-green-400 to-yellow-600">SideBar</div>
+    <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-gradient-to-br from-blue-400 via-green-300 to-yellow-300">Content  One</div>
+    <div className="bg-gradient-to-r from-pink-500 to-yellow-300">Content  Two</div>
+    <div className="bg-gradient-to-br from-purple-500 via-orange-400 to-yellow-300">Content  Three</div>
+    <div className="col-span-3  sm:col-span-2 md:col-span-3 bg-gradient-to-br from-red-300 via-pink-400 to-yellow-300">Footer</div>
+  </div>
+</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    
   );
 }
+    // Grid
+    // <div className="grid grid-cols-3 grid-rows-[200px_200px] gap-6">
+    //   <div className="bg-green-400 col-span-3">BOX-1</div>
+    //   <div className="bg-green-400 row-span-2">BOX-2</div>
+    //   <div className="bg-green-400 col-span-2">BOX-3</div>
+    // </div>
+
+    
+    //postions
+    //relative,absoulate,fixed and sticky
+   
+  //   <div>
+  //   <div className="w-80 h-80 bg-red- content-center text-center text-black bg-green-700">Box-1</div>
+  //   <div className="w-80 h-80 bg-purple-600">Box-2</div>
+  //   <div className="w-80 h-80 bg-yellow-600">Box-3</div>
+  //   <div className="w-80 h-80 bg-orange-500">Box-3</div>
+  // </div>
+
+  //assigment Grid
+//   <div className="grid grid-rows-[100px_200px_200px_100px] gap-2 text-white text-[30px] content-center p-3 bg-black text-center font-extrabold">
+//   <div className="bg-red-400 col-span-3">Header</div>
+//   <div className="bg-yellow-400 row-span-2">SideBar</div>
+//   <div className="bg-purple-400 col-span-2">Content 1</div>
+//   <div className="bg-green-400">Content 2</div>
+//   <div className="bg-orange-400">Content 3</div>
+//   <div className="bg-pink-400 col-span-3">Footer</div>
+// </div>
